261579d3281f3a61ccd28aaae9b22942133a9e39
Jackson <pertusatti22@gmail.com>
2022-10-18 19:21:29 -0300

    Test todo_repository_test.dart unitary test of todo_repository.dart using mocktail

--- lib/src/repositories/todo_repository.dart
+++ lib/src/repositories/todo_repository.dart
@@ -1,15 +1,25 @@
 import 'package:dio/dio.dart';
-import 'package:to_do_list/src/models/todo_model.dart';
+
+import '../models/todo_model.dart';
 
 class TodoRepository {
-  final dio = Dio();
-  final url = 'https://jsonplaceholder.typicode.com/todos';
+  late Dio dio;
+  final String url = 'https://jsonplaceholder.typicode.com/todos';
+
+  TodoRepository([Dio? client]) {
+    if (client == null) {
+      dio = Dio();
+    } else {
+      dio = client;
+    }
+  }
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
     final list = response.data as List;
 
     List<TodoModel> todos = [];
+
     for (var json in list) {
       final todo = TodoModel.fromJson(json);
       todos.add(todo);
